5da7dc6c73d9005d30a395b5427506e93486acb7
Dmitry Sorokin <karech92@gmail.com>
2015-03-10 17:18:58 +0300

    load csrf cookie name from django settings

--- nexus/conf.py
+++ nexus/conf.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 MEDIA_PREFIX = getattr(settings, 'NEXUS_MEDIA_PREFIX', '/nexus/media/')
+CSRF_COOKIE_NAME = getattr(settings, 'CSRF_COOKIE_NAME', 'csrftoken')
 
 if getattr(settings, 'NEXUS_USE_DJANGO_MEDIA_URL', False):
     MEDIA_PREFIX = getattr(settings, 'MEDIA_URL', MEDIA_PREFIX)
